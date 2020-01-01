3e41e8a4236450151fcc02018d9d5decfa57a470
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-01-01 22:35:30 +0100

    looping

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const data = ['cup','spoon','plate','saucer','caserole','napkin'];
+data.forEach(console.log);
+
+// cup 0 [ 'cup', 'spoon', 'plate', 'saucer', 'caserole', 'napkin' ]
+// spoon 1 [ 'cup', 'spoon', 'plate', 'saucer', 'caserole', 'napkin' ]
+// plate 2 [ 'cup', 'spoon', 'plate', 'saucer', 'caserole', 'napkin' ]
+// saucer 3 [ 'cup', 'spoon', 'plate', 'saucer', 'caserole', 'napkin' ]
+// caserole 4 [ 'cup', 'spoon', 'plate', 'saucer', 'caserole', 'napkin' ]
+// napkin 5 [ 'cup', 'spoon', 'plate', 'saucer', 'caserole', 'napkin' ]
+
+for(const item of data){
+    console.log(item);
+}
+
+// cup
+// spoon
+// plate
+// saucer
+// caserole
+// napkin
